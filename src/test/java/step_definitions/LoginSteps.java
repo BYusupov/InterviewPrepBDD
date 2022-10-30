c54c619130e1299ccd9c8f243242af8e47a25cf3
Burkhon <burkhon85@gmail.com>
2022-10-30 11:05:53 -0400

    added first test case

---
+++ src/test/java/step_definitions/LoginSteps.java
@@ -0,0 +1,37 @@
+package step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import pages.CommonPage;
+import pages.LoginPage;
+import utils.BrowserUtils;
+
+import java.util.Map;
+
+public class LoginSteps implements CommonPage {
+    LoginPage page;
+    public LoginSteps(){
+    page = new LoginPage();
+    }
+
+
+    @When("user enters {string} as {string}")
+    public void user_enters_as(String value, String placeHolder) {
+        BrowserUtils.sendKeys(BrowserUtils.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_INPUT_FIELD, placeHolder))),value);
+    }
+
+
+
+    @When("user clicks {string} button")
+        public void user_clicks_button (String logInBtn){
+            BrowserUtils.click(BrowserUtils.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_BUTTON,logInBtn))));
+        }
+        @Then("verify the title of the page")
+        public void verify_the_title_of_the_page () {
+            BrowserUtils.assertEquals(BrowserUtils.getDriver().getTitle(), "Interview App");
+        }
+    }
+
+
