40fd932e6aab94a58f38d3100b27b42ecaf94e82
Burkhon <burkhon85@gmail.com>
2022-10-30 13:36:00 -0400

    half way through USIN-8

---
+++ src/test/java/pages/SoftSkillsPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.BrowserUtils;
+
+import javax.xml.xpath.XPath;
+
+public class SoftSkillsPage {
+    public SoftSkillsPage(){
+        PageFactory.initElements(BrowserUtils.getDriver(), this);
+    }
+    @FindBy(xpath ="//input[@name='search']")
+    public WebElement searchbox;
+    @FindBy(xpath ="//button[@type='submit']")
+    public WebElement submit;
+}
