e95179f12f7601ed8728335fed89014627acd64e
Oksana Skrynnik <oksanaskrynnik@oksanas-mbp.mynetworksettings.com>
2022-10-30 13:42:41 -0400

    UPDATE1- oksana-- USIN-3

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         glue = "step_definitions",
         stepNotifications = true,
         dryRun = false
-        ,tags = "@USIN-1"
+        ,tags = "@USIN-3"
 
 )
 public class CukesRunner {
