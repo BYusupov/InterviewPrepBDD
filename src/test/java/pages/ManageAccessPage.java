bf7c6a79623ab8225b30d0b1f5415e3e7550861c
RATCHANOBON IEMSAWAT <anneratchanobon@RATCHANOBONsMBP.fios-router.home>
2022-10-31 07:33:43 -0400

    Add Manage Access Page

---
+++ src/test/java/pages/ManageAccessPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.BrowserUtils;
+
+public class ManageAccessPage {
+    public ManageAccessPage(){
+
+        PageFactory.initElements(BrowserUtils.getDriver(), this);
+    }
+    @FindBy(id = "Select-filter")
+    public WebElement selectFilter;
+}
