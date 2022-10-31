67d662fc416fb4877427c3692895fc8bbba19b11
D Alexandra <alexandra.davlitkireeva@gmail.com>
2022-10-30 21:34:13 -0400

    fix my US

---
+++ src/test/java/step_definitions/CodingSteps.java
@@ -0,0 +1,33 @@
+package step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import pages.CodingPage;
+import pages.CommonPage;
+import utils.BrowserUtils;
+
+public class CodingSteps implements CommonPage {
+    CodingPage page;
+
+    public CodingSteps() {
+        this.page = new CodingPage();
+    }
+    @When("user adds question {string} as {string}")
+    public void userAddsQuestionAs(String value, String placeholder) {
+        BrowserUtils.sendKeys(BrowserUtils.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_TEXTAREA_FIELD, placeholder))), value);
+    }
+
+    @Then("verify question {string} is displayed")
+    public void verifyQuestionIsDisplayed(String question) {
+        BrowserUtils.isDisplayed(BrowserUtils.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_TEXT, question))));
+
+    }
+
+    @Then("Verify link text {string} is displayed")
+    public void verifyLinkTextIsDisplayed(String inputTxt){
+        BrowserUtils.assertEquals(BrowserUtils.getDriver().findElement(
+                By.xpath(String.format(XPATH_TEMPLATE_BUTTON, inputTxt))).getText(), "Array reverse");
+
+    }
+}
--- src/test/java/step_definitions/HomeSteps.java
+++ src/test/java/step_definitions/HomeSteps.java
@@ -20,27 +20,6 @@ public class HomeSteps implements CommonPage {
         page = new HomePage();
         loginPage = new LoginPage();
     }
-    @Given("User opens Url")
-    public void userOpensUrl() {
-        BrowserUtils.getDriver();
-    }
 
-    @When("user adds question {string}")
-    public void user_adds_question(String input) {
-        BrowserUtils.sendKeys(page.inputField, input);
-    }
-
-    @Then("verify question {string} is displayed")
-    public void verifyQuestionIsDisplayed(String question) {
-        BrowserUtils.isDisplayed(BrowserUtils.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_TEXT, question))));
-
-    }
-
-    @Then("Verify link text {string} is displayed")
-    public void verifyLinkTextIsDisplayed(String dashboardBtns){
-            BrowserUtils.isDisplayed(BrowserUtils.getDriver().findElement(
-                    By.xpath(String.format(XPATH_TEMPLATE_BUTTON, dashboardBtns))));
-
-    }
 }
 
