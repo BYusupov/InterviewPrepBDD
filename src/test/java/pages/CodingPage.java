088853ebd804624aac2607175c66bb591d3a96e6
D Alexandra <alexandra.davlitkireeva@gmail.com>
2022-10-31 21:51:03 -0400

    Worked on USIN-6

--- src/test/java/pages/CodingPage.java
+++ src/test/java/pages/CodingPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.BrowserUtils;
 
@@ -7,4 +9,14 @@ public class CodingPage {
     public CodingPage(){
         PageFactory.initElements(BrowserUtils.getDriver(), this);
     }
+
+    @FindBy(xpath = "//p[contains(text(), 'Array')]/following::button[1]")
+    public WebElement deleteBtn;
+
+    @FindBy(xpath = "//p[contains(text(), 'Array')]/following::button[2]")
+    public WebElement editBtn;
+
+    @FindBy(xpath = "//button[@class='btn btn-sm-outline-success']")
+    public WebElement checkBtn;
+
 }
